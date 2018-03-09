05bb0b93ef63590b871a65450281dcbff5b574aa
huangfeng <huangfeng@163.com>
2018-03-09 14:36:39 +0800

    Initial commit

---
+++ wordgame_1.py
@@ -0,0 +1,12 @@
+print("------------黄锋你好---------")
+temp = input("不妨猜一下我现在心里想的是哪个数字:")
+guess = int(temp)
+if guess == 8:
+    print("fuck,you is the chongzi of huangfeng!")
+    print("..... no thanks of guessed!")
+else:
+    print("猜错啦，他现在心里想的是s!")
+print("游戏结束，不玩啦^_^")
+
+
+
