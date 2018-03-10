31220e1561d8bc64d489ec3576b91c1180323e98
huangfeng <huangfeng@163.com>
2018-03-10 09:46:18 +0800

    while_random

---
+++ wordgmae_2.py
@@ -0,0 +1,17 @@
+import random
+secret = random.randint(1,10)
+print('---------my my----------')
+temp = input("猜数字1-10的随机数：")
+guess = int(temp)
+while guess != secret:
+    temp = input("猜错了，请重新输入：")
+    guess = int(temp);
+    if guess == 8:
+        print("这都能猜中")
+        print("猜中也没啥")
+    else:
+        if guess > 8:
+            print("哥，大了大了~~~")
+        else:
+            print("小了，小了~~~~")
+print("游戏结束，不玩啦……")
