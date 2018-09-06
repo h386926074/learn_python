752296011987a268b496c216c01eea9dbe29b7bd
daniel <h386926074@163.com>
2018-09-06 17:59:36 +0800

    music

--- text_music.py
+++ text_music.py
@@ -3,6 +3,9 @@ from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
 
+from selenium import webdriver
+import csv
+
 html = urlopen('https://music.163.com/#/discover/playlist')
 
 bs_obj = BeautifulSoup(html.read(),'html.parser')
@@ -13,3 +16,25 @@ for text in text_list:
     print(text.get_text())
 
 html.close()
+
+
+url = 'https://music.163.com/#/discover/playlist/?order=hot&cat=%E5%85%A8%E9%83%A8&limit=35&offset=0'
+driver = webdriver.PhantomJS()
+
+csv_file = open('playlist03.csv','w',newline='')
+writer = csv.writer(csv_file)
+writer.writerow(['标题','播放数','链接'])
+
+while url != 'javascript:void(0)':
+    driver.get(url)
+    driver.switch_to.frame('contentFrame')
+    data = driver.find_element_by_id('m-pl-container').find_elements_by_tag_name('li')
+    for i in range(len(data)):
+        datasoup = data[i]
+        nb = datasoup.find_element_by_class_name('nb').text
+        if '万' in nb and int(nb.split('万')[0]) > 500:
+            msk = datasoup.find_element_by_css_selector('a.msk')
+            writer.writerow([msk.get_attribute('title'),nb,msk.get_attribute('href')])
+
+    url = driver.find_element_by_css_selector('a.zbtn.znxt').get_attribute('href')
+csv_file.close()
